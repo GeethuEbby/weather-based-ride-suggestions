5ceeca1344060467efb065a4abe87e0fc36245b7
GeethuEbby <110143831+GeethuEbby@users.noreply.github.com>
2022-09-15 01:11:31 -0300

    Update prepare_map.py

--- prepare_map.py
+++ prepare_map.py
@@ -13,7 +13,7 @@ from folium.plugins import MarkerCluster
 # In[2]:
 
 
-def map_html(lat,lng,gas_veh_subset,fuel_type,p_name,dist):
+def map_html(lat,lng,gas_veh_subset,fuel_type,p_name,dist,ex_veh_set):
     m = folium.Map(location=[lat, lng], zoom_start=15)
 
     
@@ -21,7 +21,12 @@ def map_html(lat,lng,gas_veh_subset,fuel_type,p_name,dist):
     for _, row in gas_veh_subset.iterrows():
         folium.Marker(location=[row["lat"], row["lon"]],
                       popup=row["driver_name"]+"---"+row["vehicle_type"]+"---"+row["fuel_type"], 
-                      icon= folium.Icon(icon="car",prefix="fa",color="red", icon_color='lightblue')).add_to(marker_cluster)
+                      icon= folium.Icon(icon="car",prefix="fa",color="green", icon_color='lightblue')).add_to(marker_cluster)
+    
+    for _, erow in ex_veh_set.iterrows():
+        folium.Marker(location=[erow["lat"], erow["lon"]], 
+                      icon= folium.Icon(icon="car",prefix="fa",color="red", icon_color='lightblue')).add_to(m)
+
 
     folium.Circle (location=[lat, lng], radius=dist, fill_color='blue').add_to(m)
     folium.Marker(location=[lat, lng],
@@ -30,4 +35,3 @@ def map_html(lat,lng,gas_veh_subset,fuel_type,p_name,dist):
 
     m.save('maps/avail_'+fuel_type+'_veh.html')   
     return
-    
