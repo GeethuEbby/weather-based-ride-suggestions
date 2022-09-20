e27b8590608c11a0e480bb151f15d409ad7af49f
GeethuEbby <110143831+GeethuEbby@users.noreply.github.com>
2022-09-20 01:08:16 -0300

    Update prepare_map.py

--- prepare_map.py
+++ prepare_map.py
@@ -14,18 +14,38 @@ from folium.plugins import MarkerCluster
 
 
 def map_html(lat,lng,gas_veh_subset,fuel_type,p_name,dist,ex_veh_set):
-    m = folium.Map(location=[lat, lng], zoom_start=14)
+    m = folium.Map(location=[lat, lng], zoom_start=15)
 
     
     marker_cluster = MarkerCluster().add_to(m)
-    for _, row in gas_veh_subset.iterrows():
-        folium.Marker(location=[row["lat"], row["lon"]],
-                      popup=row["driver_name"]+"---"+row["vehicle_type"]+"---"+row["fuel_type"], 
-                      icon= folium.Icon(icon="car",prefix="fa",color="green", icon_color='lightblue')).add_to(marker_cluster)
     
-    for _, erow in ex_veh_set.iterrows():
-        folium.Marker(location=[erow["lat"], erow["lon"]], 
-                      icon= folium.Icon(icon="car",prefix="fa",color="red", icon_color='lightblue')).add_to(m)
+    for _, row in ex_veh_set.iterrows():
+        if (row["vehicle_type"] == 'moped'): 
+            folium.Marker(location=[row["lat"], row["lon"]], 
+                      popup=row["vehicle_type"],
+                      icon= folium.Icon(icon="bicycle",prefix="fa",color="red", icon_color='lightgoldenrodyellow')).add_to(m)
+        if (row["vehicle_type"] == 'motorcycle'): 
+            folium.Marker(location=[row["lat"], row["lon"]], 
+                      popup=row["vehicle_type"],
+                      icon= folium.Icon(icon="motorcycle",prefix="fa",color="red", icon_color='lightcoral')).add_to(m)
+        if (row["vehicle_type"].startswith('passenger')): 
+            folium.Marker(location=[row["lat"], row["lon"]], 
+                      popup="private",
+                      icon= folium.Icon(icon="bus",prefix="fa",color="red", icon_color='lightblue')).add_to(m)
+
+            
+    
+    for _, row in gas_veh_subset.iterrows():
+        if (row["vehicle_type"] == 'taxi'): 
+            folium.Marker(location=[row["lat"], row["lon"]],
+                      popup=row["driver_name"]+"---"+row["vehicle_type"]+"---"+row["fuel_type"], 
+                      icon= folium.Icon(icon="taxi",prefix="fa",color="lightgreen", icon_color='darkblue')).add_to(marker_cluster)
+        if (row["vehicle_type"] == 'uber'): 
+            folium.Marker(location=[row["lat"], row["lon"]],
+                      popup=row["driver_name"]+"---"+row["vehicle_type"]+"---"+row["fuel_type"], 
+                      icon= folium.Icon(icon="car",prefix="fa",color="darkgreen", icon_color='lightblue')).add_to(marker_cluster)   
+    
+
 
 
     folium.Circle (location=[lat, lng], radius=dist, fill_color='blue').add_to(m)
